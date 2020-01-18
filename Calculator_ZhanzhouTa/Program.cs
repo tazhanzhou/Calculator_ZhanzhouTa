0e46d2a9aa0f9bfc389fad2c01b0a32369c31dfe
Zhanzhou Ta <tazhanzhou@outlook.com>
2020-01-17 19:54:24 -0500

    first

--- Calculator_ZhanzhouTa/Program.cs
+++ Calculator_ZhanzhouTa/Program.cs
@@ -8,8 +8,18 @@ namespace Calculator_ZhanzhouTa
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            decimal input1;
+            decimal input2;
+            Console.WriteLine("Please input the first Number.");
+            validater the input
+            input1 = (Decimal)Console.ReadLine();
+
+
+
         }
     }
+    
 }
