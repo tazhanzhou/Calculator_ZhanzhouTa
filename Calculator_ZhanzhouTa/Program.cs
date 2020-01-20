bfab20709e95fde473a0b27dd2cc2df2a1c22de5
Zhanzhou Ta <6151957@johnabbottcollege.net>
2020-01-19 21:02:52 -0500

    before submitt

--- Calculator_ZhanzhouTa/Program.cs
+++ Calculator_ZhanzhouTa/Program.cs
@@ -11,15 +11,63 @@ namespace Calculator_ZhanzhouTa
 
         static void Main(string[] args)
         {
+            Boolean c = true;
             decimal input1;
             decimal input2;
-            Console.WriteLine("Please input the first Number.");
-            validater the input
-            input1 = (Decimal)Console.ReadLine();
+            String funct;
+            while(c == true)
+            {
+                Console.WriteLine("Please input the First Number.");
+                //validater the input
+                input1 = Convert.ToDecimal(Console.ReadLine());
+                Console.WriteLine("Please select the Function + - * / .");
+                funct = Console.ReadLine();
+                Console.WriteLine("Please input the Second Number.");
+                input2 = Convert.ToDecimal(Console.ReadLine());
 
+                if (funct == "/"&& input2 == 0){
+                    Console.WriteLine("Dividing by zero, pleae re-input the second number");
+                    input2 = Convert.ToDecimal(Console.ReadLine());
+                }
 
+                getResault(funct,input1,input2);
 
+                Console.WriteLine("Do you want continue? y/n");
+                String ifContinue = Console.ReadLine();
+                if(ifContinue == "n")
+                {
+                c=false;
+                    break;
+                }
+                
+
+            }
         }
+        static void getResault( string funct,decimal input1,decimal input2)
+        {
+            decimal res = 0;
+            string symble = "";
+            switch (funct)
+            {
+                case "+":
+                    res = input1+input2;
+                    symble = "+";
+                    break;
+                case "-":
+                    res = input1-input2;
+                    symble = "-";
+                    break;
+                case "x":
+                    res = input1*input2;
+                    symble = "x";
+                    break;
+                case "/":
+                    res = input1/input2;
+                    symble = "/";
+                    break;
+            }
+        Console.WriteLine("{0}{2}{1}={3}",input1,input2,symble,res);
+        }    
     }
     
 }
